afe0272025b990c353351b56d7fd93747edd1dcb
kadirzkaya <bmkozkaya@gmail.com>
2022-09-20 12:25:17 +0300

    Harmonik seri ortalama hesaplama

--- src/HarmonikSeri.java
+++ src/HarmonikSeri.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class HarmonikSeri {
+public class HarmonikSeri {
+    public static void main(String[] args) {
+        double result=0.0;
+        int [] dizi={4,7,2,8,12,3};
+        for(int i:dizi){
+            result+=1.0/i;
+        }
+
+        System.out.println(dizi.length/result);
+    }
 }
